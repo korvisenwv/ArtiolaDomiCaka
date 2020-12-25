cef1cbd523ec89a453739cb931bcb25b04c19804
korvisenwv <aaqwe5926@sina.com>
2020-12-25 17:30:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let usBigMacPrice=5.66;
+let switzerlandBigMacPrice=7.30;
+let switzerlandBigMacPricePPP= switzerlandBigMacPrice/usBigMacPrice;
+let todayExchangeRateCHFDollar= 0.92;
+let switzerlandIndexBigMacPrice= ((switzerlandBigMacPricePPP /todayExchangeRateCHFDollar)/todayExchangeRateCHFDollar)*100;
+
+
+
+let swedenBigMacPrice=6.37;
+let swedenBigMacPricePPP= swedenBigMacPrice/usBigMacPrice;
+let todayExchangeRateSEKDollar= 8.79;
+let swedenIndexBigMacPrice= ((swedenBigMacPricePPP /todayExchangeRateSEKDollar)/todayExchangeRateSEKDollar)*100;
+
+let norwayBigMacPrice=6.09;
+let norwayBigMacPricePPP= norwayBigMacPrice/usBigMacPrice;
+let todayExchangeRateNOKDollar= 8.71;
+let norwayIndexBigMacPrice= ((norwayBigMacPricePPP /todayExchangeRateNOKDollar)/todayExchangeRateNOKDollar)*100;
+
+
+let israelBigMacPrice=5.35;
+let israelBigMacPricePPP= israelBigMacPrice/usBigMacPrice;
+let todayExchangeRateISLDollar= 3.10;
+let israelIndexBigMacPrice= ((israelBigMacPricePPP /todayExchangeRateISLDollar)/todayExchangeRateISLDollar)*100;
+
+
+
+let canadaBigMacPrice=5.29;
+let canadaBigMacPricePPP= canadaBigMacPrice/usBigMacPrice;
+let todayExchangeRateCADDollar= 1.25;
+let canadaIndexBigMacPrice= ((canadaBigMacPricePPP /todayExchangeRateCADDollar)/todayExchangeRateCADDollar)*100;
